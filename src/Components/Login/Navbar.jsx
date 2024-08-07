8a0fd0affeb75d681df2407f9f9da6a5327b5e95
DikshaPali72 <dikshapali199@gmail.com>
2024-08-07 15:13:19 +0530

    update

--- src/Components/Login/Navbar.jsx
+++ src/Components/Login/Navbar.jsx
@@ -6,8 +6,8 @@ const Navbar = () => {
   return (
   <>
    <div className='bg-slate-600 p-4 text-white'>
-    <div  className='container mx-auto flex justify-between items-center '>
-    <img  onClick={()=>{navigate("/")}} src={logo} alt="Travel Logo" className="h-14 w-56 " />
+    <div  className='container mx-auto flex justify-between items-center  '>
+    <img  onClick={()=>{navigate("/")}} src={logo} alt="Travel Logo" className="h-14 w-56  " />
     <div className='  flex gap-4'>
 
        <button onClick={()=>{navigate("/ContactUsPage")}}> Contact Us</button> 
